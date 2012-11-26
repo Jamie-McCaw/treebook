d49227c0796c9ca82fdd0dbe18a93feb6ab6ce97
Jamie-McCaw <psiral@yahoo.com>
2012-11-26 11:32:31 -0500

    Fixed with user profile validation

--- test/unit/user_test.rb
+++ test/unit/user_test.rb
@@ -35,4 +35,12 @@ class UserTest < ActiveSupport::TestCase
 		assert !user.errors[:profile_name].empty?
 		assert user.errors[:profile_name].include?("Must be formatted correctly.")
 	end
+
+	test "a user can have a correctly formatted profile name" do
+		user = User.new(first_name: 'Jamie', last_name: 'McCaw', email: 'psiral@yaho.com')
+		user.password = user.password_confirmation = "asdfasdf"
+
+		user.profile_name = 'JamieMcCaw_1'
+		assert user.valid?
+	end
 end
