fcf0dbda91496e690b96da5a2295851de2362154
Jamie-McCaw <psiral@yahoo.com>
2012-11-26 09:10:07 -0500

    Styled flash messages

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,13 @@
 module ApplicationHelper
+
+	def flash_class(type)
+		case type 
+		when :alert
+			return "alert-error"
+		when :notice
+			return "alert-success"
+		else
+			 ""
+		end
+	end
 end
